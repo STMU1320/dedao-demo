821c7ec6468da038bbb94bec501d1148dcc6e199
stmu <904574519@qq.com>
2017-10-30 12:37:30 +0800

    修复import大小写警告

--- src/views/home/c/BookBody.js
+++ src/views/home/c/BookBody.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header } from 'components/section'
+import { Header } from 'components/Section'
 import { currency, timeFormat } from 'utils'
 import styles from '../style.less'
 
